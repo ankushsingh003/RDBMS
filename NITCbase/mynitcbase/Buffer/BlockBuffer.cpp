c92b18422cbc86b1f5f4c5836fe46aaf8faef1b6
Ankush Singh <rathorankushrana143@gmail.com>
2026-03-22 07:41:13 +0530

    Complete Stage 2: Implemented catalog reading and display logic

--- NITCbase/mynitcbase/Buffer/BlockBuffer.cpp
+++ NITCbase/mynitcbase/Buffer/BlockBuffer.cpp
@@ -1,4 +1,39 @@
 #include "BlockBuffer.h"
-
-#include <cstdlib>
 #include <cstring>
+
+// Constructor for BlockBuffer
+BlockBuffer::BlockBuffer(int blockNum) {
+  this->blockNum = blockNum;
+}
+
+// Get the header of the block
+int BlockBuffer::getHeader(struct HeadInfo *head) {
+  unsigned char buffer[BLOCK_SIZE];
+  Disk::readBlock(buffer, this->blockNum);
+  memcpy(head, buffer, HEADER_SIZE);
+  return SUCCESS;
+}
+
+// Constructor for RecBuffer
+RecBuffer::RecBuffer(int blockNum) : BlockBuffer(blockNum) {}
+
+// Get the record at a specific slotNum
+int RecBuffer::getRecord(union Attribute *rec, int slotNum) {
+  unsigned char buffer[BLOCK_SIZE];
+  Disk::readBlock(buffer, this->blockNum);
+
+  struct HeadInfo head;
+  this->getHeader(&head);
+
+  int numAttrs = head.numAttrs;
+  int numSlots = head.numSlots;
+
+  // Offset of record = HEADER_SIZE + numSlots + (slotNum * numAttrs * ATTR_SIZE)
+  // Each slot has one byte in the slotmap
+  int recordSize = numAttrs * ATTR_SIZE;
+  unsigned char *slotPointer = buffer + HEADER_SIZE + numSlots + (slotNum * recordSize);
+  
+  memcpy(rec, slotPointer, recordSize);
+
+  return SUCCESS;
+}
--- NITCbase/mynitcbase/main.cpp
+++ NITCbase/mynitcbase/main.cpp
@@ -6,21 +6,39 @@
 #include <cstring>
 
 int main(int argc, char *argv[]) {
-  /* Initialize the Run Copy of Disk */
   Disk disk_run;
 
-  unsigned char buffer[BLOCK_SIZE];
-  Disk::readBlock(buffer, 7000);
-  char message[] = "hello";
-  memcpy(buffer + 20, message, 6);
-  Disk::writeBlock(buffer, 7000);
+  RecBuffer relCatBlock(RELCAT_BLOCK);
 
-  unsigned char buffer2[BLOCK_SIZE];
-  char message2[6];
-  Disk::readBlock(buffer2, 7000);
-  memcpy(message2, buffer2 + 20, 6);
-  std::cout << message2 << std::endl;
+  HeadInfo relCatHeader;
+  relCatBlock.getHeader(&relCatHeader);
 
-  // return FrontendInterface::handleFrontend(argc, argv);
+  for (int i = 0; i < relCatHeader.numEntries; i++) {
+
+    Attribute relCatRecord[RELCAT_NO_ATTRS]; // RELCAT_NO_ATTRS = 6
+    relCatBlock.getRecord(relCatRecord, i);
+
+    printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
+
+    RecBuffer attrCatBlock(ATTRCAT_BLOCK);
+
+    HeadInfo attrCatHeader;
+    attrCatBlock.getHeader(&attrCatHeader);
+
+    for (int j = 0; j < attrCatHeader.numEntries; j++) {
+
+      Attribute attrCatRecord[ATTRCAT_NO_ATTRS]; // ATTRCAT_NO_ATTRS = 6
+      attrCatBlock.getRecord(attrCatRecord, j);
+
+      if (strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal, 
+                 relCatRecord[RELCAT_REL_NAME_INDEX].sVal) == 0) {
+        
+        const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
+        printf("  %s: %s\n", attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrType);
+      }
+    }
+    printf("\n");
+  }
+
   return 0;
 }
